487fb6858dd10dcab3f64a8318f66124e181742d
vovadener <vovanpokatashkin548@gmail.com>
2026-01-30 02:02:45 +0300

    add JsonCreator annotation

--- src/main/java/model/subclasses/BaseFootballerCharacteristics.java
+++ src/main/java/model/subclasses/BaseFootballerCharacteristics.java
@@ -1,25 +1,38 @@
 package model.subclasses;
 
 
-import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class BaseFootballerCharacteristics {
-    private short[] _characteristics;
+    private ArrayList<Short> _characteristics;
 
-    public BaseFootballerCharacteristics() {
-        _characteristics = new short[FootballerCharacteristicsEnum.cnt];
-    }
+	public BaseFootballerCharacteristics(ArrayList<Short> characteristics) {
+		_characteristics = characteristics;
+	}
+
+	@JsonCreator
+	public BaseFootballerCharacteristics(Map<String, Short> characteristics) {
+		characteristics.forEach((key, value) -> {
+			int pos = FootballerCharacteristicsEnum.valueOf(key).array_pos;
+			_characteristics.set(pos, value);
+		});
+	}
 
     public short characteristic(FootballerCharacteristicsEnum characteristic) {
-        return _characteristics[characteristic.array_pos];
+        return _characteristics.get(characteristic.array_pos);
     }
 
-	@JsonSetter
     public void increaseCharacteristic(FootballerCharacteristicsEnum characteristic, short add) {
-        _characteristics[characteristic.array_pos] += add;
+        _characteristics.set(characteristic.array_pos, (short) (_characteristics.get(characteristic.array_pos) + add));
     }
 
     public void decreaseCharacteristic(FootballerCharacteristicsEnum characteristic, short loss) {
-        _characteristics[characteristic.array_pos] -= loss;
+		short currentValue = _characteristics.get(characteristic.array_pos);
+		_characteristics.set(characteristic.array_pos, (short) (currentValue - Math.min(currentValue, loss)));
     }
 }
